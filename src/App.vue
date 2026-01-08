09af44e4aabb2ebaaef198030a9551c8c9d8ce54
Marcus V. N. Carraschi <marcuscarraschy@gmail.com>
2026-01-07 22:11:51 -0300

    Initial: landing page base

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+
+</script>
+
+<template>
+  
+  <header>
+    <h1>My page</h1>
+  </header>
+  
+  <main>
+    <p>Hi there =D</p>
+  </main>
+</template>
+
+<style scoped>
+
+  header {
+    background-color: #42b883;
+    color: white;
+    padding: 20px;
+    text-align: center;
+  }
+</style>
